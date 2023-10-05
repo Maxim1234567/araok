5acd8bb89344a214cbc4ac72203d0c6cd6a5cf30
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-10-05 21:36:44 +0300

    user service

---
+++ UserMS/src/main/java/ru/araok/domain/User.java
@@ -0,0 +1,37 @@
+package ru.araok.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String phone;
+
+    private String password;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    private String role;
+}
