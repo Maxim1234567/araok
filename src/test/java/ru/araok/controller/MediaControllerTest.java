681b9cc6cc0db96aafa161f696c73db3a12c35ae
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-10-18 21:00:34 +0300

    Доделал тест для контроллера MediaControllerTest

--- src/test/java/ru/araok/controller/MediaControllerTest.java
+++ src/test/java/ru/araok/controller/MediaControllerTest.java
@@ -1,13 +1,16 @@
 package ru.araok.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.araok.dto.AgeLimitDto;
 import ru.araok.dto.ContentDto;
+import ru.araok.dto.ContentMediaDto;
 import ru.araok.dto.ContentMediaIdDto;
 import ru.araok.dto.LanguageDto;
 import ru.araok.dto.MediaTypeDto;
@@ -32,11 +35,55 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(MediaController.class)
 public class MediaControllerTest {
 
+    private static final String JSON_CONTENT_MEDIA =
+            """
+                            {
+                                "contentMediaId": {
+                                    "content": {
+                                        "id": 1,
+                                        "name": "Unknown Content",
+                                        "limit": {
+                                            "id": 1,
+                                            "description": "for children under 6 years of age",
+                                            "limit": 0
+                                        },
+                                        "artist": "Unknown Artist",
+                                        "user": {
+                                            "id": 1,
+                                            "name": "Maxim",
+                                            "phone": "89993338951",
+                                            "password": "12345",
+                                            "birthDate": "1994-08-05",
+                                            "role": "USER"
+                                        },
+                                        "createDate": "2023-10-18",
+                                        "language": {
+                                            "id": 1,
+                                            "language": "Russian",
+                                            "code2": "RU"
+                                        }
+                                    },
+                                    "mediaType": {
+                                        "id": 1,
+                                        "type": "VIDEO"
+                                    }
+                                },
+                                "media": "AQEBAQEBAQEBAQEBAQEBAQAAAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQABAQEBAQEBAQEBAQEBAQEBAAEBAQEBAQEBAQEBAQEBAQEBAQEBAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQABAQEBAQEBAQEBAQEBAQABAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQABAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQAAAAAAAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEAAQEAAAAAAAEBAQEBAQEAAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEAAQABAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEAAQEBAQ=="
+                            }
+                    """;
+
     @MockBean
     private MediaService mediaService;
 
     @Autowired
+    private MockMvc mvc;
+
+    @Autowired
     private ObjectMapper mapper;
+
+    private static Long CONTENT_ID = 1L;
+
+    private static Long TYPE_ID = 1L;
 
     private byte[] video = {
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -54,15 +101,84 @@ public class MediaControllerTest {
             1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1
     };
 
-    private ContentMediaIdDto contentMediaId;
+    private ContentMediaDto contentMedia;
 
     @BeforeEach
     public void setUp() {
+        AgeLimitDto limit = AgeLimitDto.builder()
+                .id(1L)
+                .description("for children under 6 years of age")
+                .limit(0L)
+                .build();
 
+        UserDto user = UserDto.builder()
+                .id(1L)
+                .name("Maxim")
+                .phone("89993338951")
+                .password("12345")
+                .birthDate(LocalDate.of(1994, 8, 5))
+                .role("USER")
+                .build();
+
+        LanguageDto language = LanguageDto.builder()
+                .id(1L)
+                .code2("RU")
+                .language("Russian")
+                .build();
+
+        ContentDto content = ContentDto.builder()
+                .id(1L)
+                .name("Unknown Content")
+                .limit(limit)
+                .artist("Unknown Artist")
+                .user(user)
+                .createDate(LocalDate.now())
+                .language(language)
+                .build();
+
+        MediaTypeDto mediaType = MediaTypeDto.builder()
+                .id(1L)
+                .type("VIDEO")
+                .build();
+
+        ContentMediaIdDto contentMediaId = ContentMediaIdDto.builder()
+                .mediaType(mediaType)
+                .content(content)
+                .build();
+
+        contentMedia = ContentMediaDto.builder()
+                .contentMediaId(contentMediaId)
+                .media(video)
+                .build();
     }
 
     @Test
-    public void shouldCorrectReturnMediaByContentIdAndTypeId() {
+    public void shouldCorrectReturnMediaByContentIdAndTypeId() throws Exception {
+        given(mediaService.findMediaByContentIdAndTypeId(eq(CONTENT_ID), eq(TYPE_ID)))
+                .willReturn(video);
 
+        mvc.perform(get("/api/media/" + CONTENT_ID + "/" + TYPE_ID)
+                        .header("Content-Type", "application/octet-stream"))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes(video));
+
+        verify(mediaService, times(1))
+                .findMediaByContentIdAndTypeId(eq(CONTENT_ID), eq(TYPE_ID));
+    }
+
+    @Test
+    public void shouldCorrectSaveContentMedia() throws Exception {
+        given(mediaService.save(any(ContentMediaDto.class)))
+                .willReturn(contentMedia);
+
+        mvc.perform(post("/api/media")
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .content(mapper.writeValueAsString(contentMedia))
+        ).andExpect(status().isCreated())
+        .andExpect(content().json(JSON_CONTENT_MEDIA));
+
+        verify(mediaService, times(1))
+                .save(any(ContentMediaDto.class));
     }
 }
