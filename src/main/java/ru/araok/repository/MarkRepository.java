d5c66779a04c14c027b45177e5cff1b28e7047b5
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-11-21 03:50:51 +0300

    Добавил настройки контента

---
+++ src/main/java/ru/araok/repository/MarkRepository.java
@@ -0,0 +1,7 @@
+package ru.araok.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.araok.domain.Mark;
+
+public interface MarkRepository extends JpaRepository<Mark, Long> {
+}
---
+++ src/test/java/ru/araok/repository/SettingRepositoryTest.java
@@ -0,0 +1,186 @@
+package ru.araok.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.araok.domain.AgeLimit;
+import ru.araok.domain.Content;
+import ru.araok.domain.Language;
+import ru.araok.domain.Mark;
+import ru.araok.domain.Setting;
+import ru.araok.domain.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SettingRepositoryTest {
+
+    private Long CONTENT_ID;
+
+    @Autowired
+    private SettingRepository settingRepository;
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Autowired
+    private MarkRepository markRepository;
+
+    private Setting setting;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @BeforeEach
+    public void setUp() {
+        AgeLimit limit = AgeLimit.builder()
+                .id(1L)
+                .description("for children under 6 years of age")
+                .limit(0L)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .name("Maxim")
+                .phone("89993338951")
+                .password("12345")
+                .birthDate(LocalDate.of(1994, 8, 5))
+                .role("USER")
+                .build();
+
+        Language language = Language.builder()
+                .id(1L)
+                .code2("RU")
+                .language("Russian")
+                .build();
+
+        Content content = Content.builder()
+                .id(1L)
+                .name("Unknown Content")
+                .limit(limit)
+                .artist("Unknown Artist")
+                .user(user)
+                .createDate(LocalDate.now())
+                .language(language)
+                .build();
+
+        Mark mark1 = Mark.builder()
+                .start(1L)
+                .end(2L)
+                .repeat(1L)
+                .delay(1L)
+                .build();
+
+        Mark mark2 = Mark.builder()
+                .start(2L)
+                .end(3L)
+                .repeat(2L)
+                .delay(2L)
+                .build();
+
+        Mark mark3 = Mark.builder()
+                .start(3L)
+                .end(4L)
+                .repeat(3L)
+                .delay(3L)
+                .build();
+
+        setting = Setting.builder()
+                .marks(List.of(
+                        mark1, mark2, mark3
+                ))
+                .content(content)
+                .build();
+
+        contentRepository.save(content);
+
+        CONTENT_ID = content.getId();
+    }
+
+    @Test
+    public void shouldCorrectSaveSetting() {
+        Setting saveSetting = settingRepository.save(setting);
+
+        Setting result = settingRepository.findById(saveSetting.getId()).get();
+
+        assertThatSetting(setting, result);
+    }
+
+    @Test
+    public void shouldCorrectUpdateSetting() {
+        Setting saveSetting = settingRepository.save(setting);
+
+        Mark newMark1 = Mark.builder()
+                .id(saveSetting.getMarks().get(0).getId())
+                .start(10L)
+                .end(20L)
+                .repeat(30L)
+                .delay(40L)
+                .build();
+
+        Mark newMark2 = Mark.builder()
+                .id(saveSetting.getMarks().get(1).getId())
+                .start(50L)
+                .end(60L)
+                .repeat(70L)
+                .delay(80L)
+                .build();
+
+        Mark newMark3 = Mark.builder()
+                        .start(90L)
+                        .end(100L)
+                        .repeat(110L)
+                        .delay(120L)
+                        .build();
+
+        Mark newMark4 = Mark.builder()
+                        .start(130L)
+                        .end(140L)
+                        .repeat(150L)
+                        .delay(160L)
+                        .build();
+
+        Setting newSetting = Setting.builder()
+                .id(saveSetting.getId())
+                .content(saveSetting.getContent())
+                .marks(List.of(newMark1, newMark2, newMark3, newMark4))
+                .build();
+
+        em.clear();
+
+        markRepository.saveAll(newSetting.getMarks());
+
+        settingRepository.save(newSetting);
+        Setting result = settingRepository.findById(newSetting.getId()).get();
+
+        assertThatSetting(newSetting, result);
+    }
+
+    private void assertThatSetting(Setting expected, Setting result) {
+        assertThatMarkList(expected.getMarks(), result.getMarks());
+    }
+
+    private void assertThatMarkList(List<Mark> expected, List<Mark> result) {
+        assertThat(result).isNotNull()
+                .hasSize(expected.size());
+
+        for(int i = 0; i < expected.size(); i++) {
+            assertThatMark(expected.get(i), result.get(i));
+        }
+    }
+
+    private void assertThatMark(Mark expected, Mark result) {
+        assertThat(result).isNotNull()
+                .matches(m -> m.getStart().equals(expected.getStart()))
+                .matches(m -> m.getEnd().equals(expected.getEnd()))
+                .matches(m -> m.getDelay().equals(expected.getDelay()))
+                .matches(m -> m.getRepeat().equals(expected.getRepeat()));
+    }
+}
